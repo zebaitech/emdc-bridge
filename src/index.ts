e3d2f9dbd9239201997ac49338c181784d3b8ee5
泽柏 <djonce@qq.com>
2026-03-16 14:37:16 +0800

    fix: relax android method typings

--- src/index.ts
+++ src/index.ts
@@ -18,9 +18,9 @@ export interface BridgeOptions {
 export type BridgeHandler = (data: unknown, raw: BridgePayload) => void;
 
 interface EmdcBridgeAndroid {
-  setItem: (payload: { key: string; data?: string | AnyObject }) => void;
-  getItem: (key: string) => unknown;
-  clearItem: (key: string) => void;  // 清除对应的数据
+  setItem: (payload?: string | AnyObject) => unknown;
+  getItem: (payload?: string | AnyObject) => unknown;
+  clearItem: (payload?: string | AnyObject) => unknown;
   getDeviceInfo: () => unknown;
 }
 
